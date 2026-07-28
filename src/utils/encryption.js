aab8b5f42543bf78dc5fcd87f5649ce64b1224c7
emmanuel sam <fndm4@gmail.com>
2026-07-28 12:57:36 +0100

    clean up

--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,13 +1,9 @@
 import CryptoJS from "crypto-js";
 
-const SECRET_KEY =
-  process.env.NEXT_PUBLIC_SECRET_KEY || "HAB_GLOBAL_SECRET";
+const SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY || "HAB_GLOBAL_SECRET";
 
 export const encryptData = (data) => {
-  return CryptoJS.AES.encrypt(
-    JSON.stringify(data),
-    SECRET_KEY
-  ).toString();
+  return CryptoJS.AES.encrypt(JSON.stringify(data), SECRET_KEY).toString();
 };
 
 export const decryptData = (cipher) => {
